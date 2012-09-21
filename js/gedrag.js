f69d5653735621d3bb3ec550c6bd1d4eed908500
DrummerHead <drummerhead@gmail.com>
2012-09-20 22:16:08 -0300

    Initial Backbone structure

--- js/gedrag.js
+++ js/gedrag.js
@@ -1,33 +1,71 @@
+var ImageData = Backbone.Model.extend({
+  defaults: {
+    captionTop: 'not sure if image',
+    captionBottom: 'or text',
+    imgSrc: 'http://mcdlr.com/i/fry.png'
+  }
+});
+
+var ImageView = Backbone.View.extend({
+  initialize: function(){
+    this.template = _.template($('#imageBinTemplate').html);
+  },
+
+  render: function(){
+    var imageContent = this.template(this.model.toJSON())
+    this.$el.html(imageContent)
+    return this;
+  }
+});
+
+
+
+
+
+var LeForm = Backbone.View.extend({
+  el: '#leForm',
+
+  events: {
+    'submit': 'getData'
+  },
+
+  getData: function(e){
+    e.preventDefault();
+    var putImgUrl = this.$el.find('#putImgUrl').val();
+    var putCaptionTop = this.$el.find('#putCaptionTop').val();
+    var putCaptionBottom = this.$el.find('#putCaptionBottom').val();
+  }
+
+});
+
+
+var Router = Backbone.Router.extend({
+  routes: {
+    '': 'landing',
+    '*magia': 'params'
+  },
+  initialize: function(){
+    var leform = new LeForm;
+    var imageData = new ImageData;
+    var imageView = new ImageView({model: 'imageData'});
+
+    $('[role="main"]').html(imageView.render().$el)
+  },
+  landing: function(){
+    console.log('esto es la landing');
+  },
+  params: function(magia){
+    console.log(magia);
+  },
+  start: function(){
+    console.log('arranco la cosa esta del router');
+    Backbone.history.start();
+  }
+});
+
+
 jQuery(function($){
-
-
-
-var $form = $('#leForm');
-var $url = $form.find('#putImgUrl');
-var $fCTop = $form.find('#putCaptionTop');
-var $fCBot = $form.find('#putCaptionBottom');
-var $eCTop = $('#captionTop');
-var $eCBot = $('#captionBottom');
-var $bin = $('#imageBin');
-var imgTail = '<img src="';
-var imgHead = '" alt="" id="memeIsMe">';
-
-$form.submit(function(e){
-  e.preventDefault();
-  var url = $url.val();
-  var tCTop = $fCTop.val();
-  var tCBot = $fCBot.val();
-  var imgReady = imgTail + url + imgHead;
-  $('#memeIsMe').remove();
-  $bin.prepend(imgReady);
- // var imgWidth = $('#memeIsMe').width();
- // $bin.width(imgWidth);
- // console.log('sisiimgWidth  = '+ imgWidth);
-  $eCTop.text(tCTop);
-  $eCBot.text(tCBot);
-})
-
-
-
+  var memeToText = new Router;
+  memeToText.start();
 });
 
